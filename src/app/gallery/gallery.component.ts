e312efc5472684c8d8f4a90b90bff5ad50ca1f8e
Ronan <ronangleeson20@gmail.com>
2024-03-02 17:48:57 +0000

    add lightgallery

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LightgalleryModule } from 'lightgallery/angular';
+import lgZoom from 'lightgallery/plugins/zoom';
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import { BeforeSlideDetail } from 'lightgallery/lg-events';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule, LightgalleryModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.scss',
 })
@@ -15,32 +17,43 @@ export class GalleryComponent {
     {
       title: 'Image 1',
       description: 'Description 1',
-      url: 'https://via.placeholder.com/150'
+      url: 'https://via.placeholder.com/150',
     },
     {
       title: 'Image 2',
       description: 'Description 2',
-      url: 'https://via.placeholder.com/150'
+      url: 'https://via.placeholder.com/150',
     },
     {
       title: 'Image 3',
       description: 'Description 3',
-      url: 'https://via.placeholder.com/150'
+      url: 'https://via.placeholder.com/150',
     },
     {
       title: 'Image 4',
       description: 'Description 4',
-      url: 'https://via.placeholder.com/150'
+      url: 'https://via.placeholder.com/150',
     },
     {
       title: 'Image 5',
       description: 'Description 5',
-      url: 'https://via.placeholder.com/150'
+      url: 'https://via.placeholder.com/150',
     },
     {
       title: 'Image 6',
       description: 'Description 6',
-      url: 'https://via.placeholder.com/150'
-    }
+      url: 'https://via.placeholder.com/150',
+    },
   ];
- }
+
+  settings = {
+    zoomFromOrigin: true,
+    thumbnail: false,
+    plugins: [lgZoom, lgThumbnail],
+  };
+  
+  onBeforeSlide = (detail: BeforeSlideDetail): void => {
+    const { index, prevIndex } = detail;
+    console.log(index, prevIndex);
+  };
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GalleryComponent } from '../gallery/gallery.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    GalleryComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
