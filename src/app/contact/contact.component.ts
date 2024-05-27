02c6d253a363fc665604b0a9500dae5a0af9d702
Ronan <ronangleeson20@gmail.com>
2024-05-27 12:52:04 +0000

    remove fa npm package and use index import instead. Redo contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,19 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faLinkedin, faVimeo } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
     CommonModule,
-    FontAwesomeModule
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  faLinkedin  = faLinkedin ;
-  faVimeo  = faVimeo;
  }
